235d12b6b35e3f936f6901b10f14a8cd87f9d63a
Hafiz Mughees <mughees605@gmail.com>
2017-03-14 12:38:43 +0500

    adding main component

--- src/index.js
+++ src/index.js
@@ -3,7 +3,13 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+// importing components//
+import Main from "./components/main"
+//
+
 ReactDOM.render(
-  <App />,
+  <div>
+  <Main />
+  </div>,
   document.getElementById('root')
 );
