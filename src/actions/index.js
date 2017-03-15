c7fe25c773930abbf2390fae84e0ec9a01b7a30b
Hafiz Mughees <mughees605@gmail.com>
2017-03-15 18:11:30 +0500

    adding two message function

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,16 +1,14 @@
 import { firebaseService } from "../service/firebase"
-
-
-
-
+import firebase from "../config/index.js"
 export var startLogin = (() => {
-  return (dispatch) => {
+  return (dispatch,getState) => {
     firebaseService.githubLogin().then()
   }
 })
 export var startLogout = (() => {
   return (dispatch) => {
-    firebaseService.githubLogout().then()
+    firebaseService.githubLogout().then((user) => {
+    })
   }
 })
 export var Login = (uid) => {
@@ -19,8 +17,23 @@ export var Login = (uid) => {
     uid
   }
 }
-export var Logout = ()=>{
+export var handleMessage = (message) => {
+  return (dispatch,getState)=>{
+    var uid = firebase.auth().currentUser;
+     firebase.database().ref().child("message").push({message:message,img:uid.photoURL}).then(()=>{
+       return firebase.database().ref().child("user/" +uid.uid+ "").push({text:message,img:uid.photoURL})
+     })
+  }
+}
+export var fetchMessage = ()=>{
+  return (dispatch,getState)=>{
+    firebase.database().ref().child("message").on("value",(snapshot)=>{
+      console.log(snapshot.val(), " fetch")
+    })
+  }
+}
+export var Logout = () => {
   return {
-    type:"LOGOUT"
+    type: "LOGOUT"
   }
 }
